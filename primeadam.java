a26e92135b7b525a34c51c4c1adee58484cdbb8b
somrajjarmos <72291512+somrajjarmos@users.noreply.github.com>
2020-10-05 18:11:43 +0530

    Update primeadam.java

--- primeadam.java
+++ primeadam.java
@@ -28,7 +28,7 @@ public class primeadam
         System.out.print("Enter a Number");
         n=sc.nextInt();
         sq1=(int)Math.pow(n,2);
-        sq2=(int)Math.pow(reverse(n),2);
+        sq2=reverse((int)Math.pow(reverse(n),2));
         if(isprime(n)==1 && sq1==sq2)
             System.out.print("Prime Adam Number");
         else
